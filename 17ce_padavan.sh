8f8ecbf65923e7ecb57e5e7081e2804fc01c15ad
opensuse <6493550@qq.com>
2017-04-14 09:29:53 +0800

    更新 17ce_padavan.sh

--- 17ce_padavan.sh
+++ 17ce_padavan.sh
@@ -48,7 +48,7 @@ start()
         init_files
         echo "create link"
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORK_DIR
-        $WORK_DIR/17ce_v3 -u mjyhj@qq.com
+        $WORK_DIR/17ce_v3 -u 6493550@qq.com
         echo "17ce Client has stated."
 }
 
@@ -59,4 +59,4 @@ stop()
         echo "17ce Client has stoped."
 }
 
-start  mjyhj@qq.com
+start  6493550@qq.com
